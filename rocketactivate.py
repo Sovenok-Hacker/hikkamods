ad9fb4fd0ac147999b0089dfe7c0a9ec75407ad3
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2023-06-15 13:05:47 +0300

    Update rocketactivate.py

--- rocketactivate.py
+++ rocketactivate.py
@@ -1,4 +1,4 @@
-# meta developer: @yummy_gay and @Duo_sova
+# meta developer: @yummy_gay и @Duo_sova
 
 import re
 from .. import loader
@@ -8,7 +8,7 @@ from tgchequeman import exceptions, activate_multicheque, parse_url
 
 logger = logging.getLogger(__name__)
 
-async def activate(code, client):
+async def activate(code, client, ll):
     bot_url = parse_url("https://t.me/tonRocketBot?start=" + code)
     try:
         await activate_multicheque(
@@ -17,40 +17,52 @@ async def activate(code, client):
             password=None
         )
     except (exceptions.ChequeFullyActivatedOrNotFound, exceptions.PasswordError) as err:
-        logger.error(f"Ошибка: {err}")
+        if "❌" in ll:
+            logger.error(f"Ошибка: {err}")
     except (exceptions.ChequeActivated,
             exceptions.ChequeForPremiumUsersOnly,
             exceptions.CannotActivateOwnCheque) as warn:
-        logger.warning(f"Предупреждение: {warn}")
+        if "⚠️" in ll:
+            logger.warning(f"Предупреждение: {warn}")
         return
     except exceptions.UnknownError as err:
-        logger.error(f"Ошибка: {err}")
+        if "❌" in ll:
+            logger.error(f"Ошибка: {err}")
         return
     except Exception as err:
-        logger.error(f"Ошибка: {err}")
+        if "❌" in ll:
+            logger.error(f"Ошибка: {err}")
+    if "✅" in ll:
+        logger.info("Чек успешно активирован!")
 
 @loader.tds
 class sh_actTonRocketModule(loader.Module):
     """Активатор чеков @tonRocketBot с автоматической подпиской (и если вы не премиум пользователь автоматически решается капча)"""
-
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "ll",
+                [],
+                "Ведение журнала",
+                validator=loader.validators.MultiChoice(["❌", "⚠️", "✅"]),
+            ),
+        )
     strings = {
         "name": "sh_actTonRocket",
     }
-
     async def client_ready(self, client, db):
         self.client = client
         await client.send_message('tonRocketBot', '/start')
 
     async def watcher(self, message):
         if not message.__class__.__name__ == "Message":
-            logger.warning("Евент не являестся сообщением, скипаем ...")
             return
         if message.raw_text and 'https://t.me/tonRocketBot?start=' in message.raw_text:
             if match := re.search(r'https://t.me/tonRocketBot\?start=([A-Za-z0-9_/]+)', message.raw_text):
-                await activate(match.group(1), self.client)
+                await activate(match.group(1), self.client, self.config["ll"])
         elif message.buttons and message.buttons[0][0].url:
             if match := re.search(r'https://t.me/tonRocketBot\?start=([A-Za-z0-9_/]+)', message.buttons[0][0].url):
-                await activate(match.group(1), self.client)
+                await activate(match.group(1), self.client, self.config["ll"])
 
     async def checkactTonRocketcmd(self, message):
         """проверить работоспособность"""
